3f9f41043f92cfe412187bf7bd356d9e97fa78d6
Boujema <vincent.boujema@gmail.com>
2019-01-03 11:05:48 +0100

    Création J03

---
+++ ex09/ft_sort_integer_table.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: viboujem <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/09 21:14:41 by viboujem          #+#    #+#             */
+/*   Updated: 2018/07/12 10:06:12 by viboujem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_integer_table(int *tab, int size)
+{
+	int		i;
+	int		l;
+	int		temp;
+
+	i = 0;
+	l = 0;
+	temp = 0;
+	while (l < size)
+	{
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+		i = 0;
+		l++;
+	}
+}
